0d7554a9570c022ae282d416550540c2adc4c5c4
Matheus <matheussantosmoura2000@gmail.com>
2023-04-20 15:57:24 -0300

    Primeira versão

---
+++ meteo-project/src/main/java/com/UpdateData/MeteoProjet/App.java
@@ -0,0 +1,41 @@
+package com.UpdateData.MeteoProjet;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.github.lalyos.jfiglet.FigletFont;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            String url = "https://api.open-meteo.com/v1/forecast?latitude=-20.27&longitude=-40.31&current_weather=true&forecast_days=1&timezone=America%2FSao_Paulo&quot";
+            HttpGet httpget = new HttpGet(url);
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            HttpEntity entity = response.getEntity();
+            String respostaJSON = EntityUtils.toString(entity);
+            // System.out.println(respostaJSON);
+            JsonObject infoMeteo = JsonParser.parseString(respostaJSON).getAsJsonObject();
+            JsonObject currentWeather = infoMeteo.getAsJsonObject("current_weather");
+            //System.out.println(currentWeather.get("temperature").getAsDouble());
+            //System.out.println(currentWeather.get("weathercode").getAsInt());
+            String asciiArt1 = FigletFont.convertOneLine(
+                    currentWeather.get("temperature").getAsString() + "°C");
+            System.out.println(asciiArt1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ meteo-project/src/test/java/com/UpdateData/MeteoProjet/AppTest.java
@@ -0,0 +1,20 @@
+package com.UpdateData.MeteoProjet;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
